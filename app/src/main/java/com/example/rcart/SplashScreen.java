4010752e2b24f20d23b9a0382bd8c227797d9d1a
alwinjain1997 <alwinjain1997@gmail.com>
2020-04-09 16:01:02 -0400

    [Alwin Jain]Splash Screen and Login Page

---
+++ app/src/main/java/com/example/rcart/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.rcart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this,LoginPage.class));
+        finish();
+    }
+}
